ac2885785f92133f5afb63e9540675db2e558e01
Lee M <lee@aptitude.co.uk>
2023-05-05 11:32:13 +0100

    Removing old project

--- BlogFodder.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BlogFodder.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
