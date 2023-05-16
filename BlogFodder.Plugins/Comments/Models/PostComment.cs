514fffef9959e0d82ed0916b3e4b5a794e32c852
Lee M <lee@aptitude.co.uk>
2023-05-16 16:29:22 +0100

    Final tweaks to plugins rendering

--- BlogFodder.Plugins/Comments/Models/PostComment.cs
+++ BlogFodder.Plugins/Comments/Models/PostComment.cs
@@ -3,7 +3,7 @@ namespace BlogFodder.Plugins.Comments.Models;
 public class PostComment
 {
     public DateTime DateCreated { get; set; }
+    public Guid PostId { get; set; }
     public string? Name { get; set; }
     public string? Comment { get; set; }
-    public bool Flagged { get; set; }
 }
--- BlogFodder.Plugins/Comments/Models/PostCommentSettings.cs
+++ BlogFodder.Plugins/Comments/Models/PostCommentSettings.cs
@@ -7,4 +7,7 @@ public class PostCommentSettings : IPluginSettings
     public bool ManuallyApproveComments { get; set; }
     public bool EnableAkismetSpamCheck { get; set; }
     public bool Enabled { get; set; }
+
+    public List<PostComment> FlaggedComments { get; set; } = new();
+    public List<PostComment> CommentsToApprove { get; set; } = new();
 }
